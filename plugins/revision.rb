5479a0614565dea41793ad603877f5aaf2422cf6
haohailong <haohailong@gmail.com>
2014-04-29 21:29:24 +0200

    revision plugin

--- plugins/revision.rb
+++ plugins/revision.rb
@@ -98,7 +98,7 @@ module Jekyll
         link = File.join('https://github.com', site['github_user'], site['github_repo'],
                          'commits', branch, post['dir_name'], post['file_name'])
       end
-      html << 'View on <a href=' + link + ' target=_blank>Github</a>'
+      html << '在 <a href=' + link + ' target=_blank>Github</a> 上查看'<br />
 
       return html
     end #render
