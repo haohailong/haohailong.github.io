1d184cc98ad136f9940b4ab496679c432a62d394
haohailong <haohailong@gmail.com>
2014-04-30 23:10:50 +0200

    customize search box

--- plugins/category_list_tag.rb
+++ plugins/category_list_tag.rb
@@ -8,7 +8,7 @@ module Jekyll
         posts_in_category = context.registers[:site].categories[category].size
         category_dir = context.registers[:site].config['category_dir']
         category_url = File.join(category_dir, category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase)
-        html << "<li class='category'><i class="fa fa-arrow-circle-right"></i><a href='/blog/categories/#{category.to_url.downcase}/'>#{category} (#{posts_in_category})</a></li>\n"
+        html << "<li class='category'><a href='/blog/categories/#{category.to_url.downcase}/'>#{category} (#{posts_in_category})</a></li>\n"
         # 上面一行取代本行代码，实现中文目录显示 html << "<li class='category'><a href='/#{category_url}/'>#{category} (#{posts_in_category})</a></li>\n"
       end
       html
